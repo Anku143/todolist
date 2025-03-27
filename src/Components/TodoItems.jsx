5583b4bbeef3ebb20f4b71b7b8cf4ca0574d9675
Ankush <ankush.r5621@gmail.com>
2025-03-27 15:29:28 +0530

    data store is pending

--- src/Components/TodoItems.jsx
+++ src/Components/TodoItems.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const TodoItems = ({ text, id, isCompleted, deleteTodo, toggle }) => {
     return (
         <div>
-            <div className="flex justify-between items-center mt-2">
-                <p onClick={() => { toggle(id) }} className={`text-white ${isCompleted ? '' : 'line-through'}`}>{text}</p>
-                <div className="flex items-center">
-                    <button className="bg-stone-500 text-white p-2 rounded-lg cursor-pointer" onClick={() => { toggle(id) }}>{!isCompleted ? 'Done' : 'Not Done'}</button>
+            <div className="flex justify-between items-center mt-2 w-full">
+                <p onClick={() => { toggle(id) }} className={`text-white w-40 block text-wrap ${isCompleted ? 'line-through' : ''}`}>{text}</p>
+                <div className="flex items-center justify-end w-40">
+                    <button className="bg-stone-500 text-white p-2 rounded-lg cursor-pointer" onClick={() => { toggle(id) }}>{!isCompleted ? 'Not Done' : 'Done'}</button>
                     <button onClick={() => { deleteTodo(id) }} className="bg-stone-500 text-white p-2 rounded-lg ml-2 cursor-pointer">Delete</button>
                 </div>
             </div>

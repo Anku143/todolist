9e863d11dd897764b18e960c408cf9c30022f779
Ankush <ankush.r5621@gmail.com>
2025-03-27 15:20:17 +0530

    local storage pending

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -11,6 +11,7 @@ const Todo = () => {
         const inputText = inputRef.current.value.trim();
 
         if (inputText === '') {
+            alert('Please enter a valid todo');
             return null;
         }
 
@@ -36,7 +37,7 @@ const Todo = () => {
                 <h1 className="text-3xl font-bold text-center text-stone-800">Todo List</h1>
             </div>
             <div className="bg-stone-700 p-4 rounded-lg mt-4 w-96">
-                <input type="text" ref={inputRef} className="w-full p-2 rounded-lg focus-visible:outline-transparent bg-white" placeholder="Add a new todo" />
+                <input type="text" ref={inputRef} required className="w-full p-2 rounded-lg focus-visible:outline-transparent bg-white" placeholder="Add a new todo" />
                 <button onClick={add} className="bg-stone-500 text-white p-2 rounded-lg w-full mt-4 cursor-pointer">Add</button>
             </div>
             <div className="bg-stone-700 p-4 rounded-lg mt-4 w-96">
